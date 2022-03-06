9cc271f40bf3ee06e91ccb9e644f1a4d9547731d
roxasenjoy <andrew.mahe14@gmail.com>
2022-03-06 12:15:46 +0100

    Mise en place de toutes les pages

---
+++ app/experiences/chocodrop.php
@@ -0,0 +1,35 @@
+<?php include('../layout/header.php'); ?>
+
+<div class="containerCompany">
+    <h3 class="titleCompany">Chocodrop</h3>
+
+    <!-- Présentation de visit & Co -->
+    <section class="companyWithImage">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quia amet at mollitia asperiores praesentium maxime dolorem dolores adipisci consequatur iusto inventore ea, et facilis exercitationem placeat quasi nemo repellendus?
+        Neque aperiam dignissimos corporis mollitia dolore excepturi vero ipsam error natus minus culpa qui sint dolorum, iure nostrum a itaque quis omnis, cupiditate porro ipsa! Natus molestias eveniet eos suscipit!
+        Sint at excepturi nemo iusto quia dolorum facilis neque optio veritatis eveniet qui magni ipsum distinctio, consectetur quas nam libero ex exercitationem. Necessitatibus sapiente illo quis facilis aut ratione ea.
+        Sed nesciunt hic maxime consectetur iure nemo odit, ipsum dicta maiores laboriosam qui libero quo quod autem veniam pariatur. Nemo optio iste doloremque! Quam voluptas aliquid molestias sunt numquam dignissimos?
+        Nobis ex eligendi commodi esse quos error provident. Earum temporibus aliquid excepturi, architecto voluptas repellendus nostrum! Similique voluptate repellat aspernatur, illo cumque tenetur dolorem eius sed reprehenderit sit rem earum.</p>
+
+        <img src="../img/les_cerises_du_cafe/logo.jpg" alt="logo les cérises du café" id="logoCerisesDuCafe">
+    </section>
+
+    <!-- Création du chocolat -->
+    <section class="cerises">
+        <h3>Création du chocolat</h3>
+        <p class="presentation">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nisi laborum ipsam excepturi et assumenda eaque! Nemo praesentium consequuntur expedita dignissimos nulla veritatis facere exercitationem consequatur, aliquam eligendi? Voluptate, ullam.
+        Aliquid molestias omnis sunt sapiente enim quam placeat quidem, accusantium iste a excepturi facere nobis iure nam repudiandae maiores inventore doloribus quos magni labore pariatur veritatis? Aspernatur dolorem ad rem?</p>
+    </section>
+
+
+    <!-- Création de la boite -->
+    <section class="cerises">
+        <h3>Création de la boite</h3>
+        <p class="presentation">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nisi laborum ipsam excepturi et assumenda eaque! Nemo praesentium consequuntur expedita dignissimos nulla veritatis facere exercitationem consequatur, aliquam eligendi? Voluptate, ullam.
+        Aliquid molestias omnis sunt sapiente enim quam placeat quidem, accusantium iste a excepturi facere nobis iure nam repudiandae maiores inventore doloribus quos magni labore pariatur veritatis? Aspernatur dolorem ad rem?</p>
+    </section>
+
+
+</div>
+
+<?php include('../layout/footer.php'); ?>
